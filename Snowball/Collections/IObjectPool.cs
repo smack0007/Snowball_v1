4b21573069b6f8f35bff2dd6237843769d3eeec9
Zachary Snow <zachary.snow@gmail.com>
2011-11-06 20:13:23 +0100

    Added ObjectPool and IObjectPool.

---
+++ Snowball/Collections/IObjectPool.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Snowball.Collections
+{
+	/// <summary>
+	/// Interface for objects which manage a collection of preallocated objects.
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	public interface IObjectPool<T>
+	{
+		/// <summary>
+		/// Returns the next inactive object from the pool.
+		/// </summary>
+		T Next();
+				
+		/// <summary>
+		/// Marks an object from the pool as inactive.
+		/// </summary>
+		/// <param name="obj"></param>
+		void Return(T obj);
+	}
+}
