34fca634c4050bc3ce03661977aa7fde2037963c
Zachary Snow <zachary.snow@gmail.com>
2012-01-15 20:09:17 +0100

    Added NextPowerOf2 method to MathHelper.

---
+++ Snowball.Tests/MathHelperTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Snowball.Tests
+{
+    [TestFixture]
+    public class MathHelperTests
+    {
+        [TestCase(0, 0)]
+        [TestCase(1, 1)]
+        [TestCase(3, 4)]
+        [TestCase(5, 8)]
+        [TestCase(10, 16)]
+        [TestCase(27, 32)]
+        [TestCase(63, 64)]
+        [TestCase(65, 128)]
+        public void NextPowerOf2TestCases(int input, int expected)
+        {
+            Assert.AreEqual(expected, (int)MathHelper.NextPowerOf2((uint)input));
+        }
+    }
+}
--- Snowball/MathHelper.cs
+++ Snowball/MathHelper.cs
@@ -18,5 +18,27 @@ namespace Snowball
 		{
 			return (float)(degrees * 0.01745329251994329576923690768489);
 		}
+
+        /// <summary>
+        /// Rounds up to the next power of 2. Returns 0 if given 0.
+        /// </summary>
+        /// <remarks>Implementation of algorithm found at "http://graphics.stanford.edu/~seander/bithacks.html#RoundUpPowerOf2".</remarks>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static uint NextPowerOf2(uint input)
+        {
+            if (input == 0)
+                return 0;
+
+            input--;
+            input |= input >> 1;
+            input |= input >> 2;
+            input |= input >> 4;
+            input |= input >> 8;
+            input |= input >> 16;
+            input++;
+
+            return input;
+        }
 	}
 }
