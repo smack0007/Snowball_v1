c30db9f6f9730e7c98937affc27f16b68d1b7f1e
Zachary Snow <zachary.snow@gmail.com>
2012-06-29 22:37:50 +0200

    Correctly implemented EmbeddedResourcesStorage.

--- Snowball/Storage/EmbeddedResourcesStorage.cs
+++ Snowball/Storage/EmbeddedResourcesStorage.cs
@@ -8,11 +8,33 @@ namespace Snowball.Storage
 	{
 		Assembly assembly;
 
+		public string BaseNamespace
+		{
+			get;
+			private set;
+		}
+
 		/// <summary>
 		/// Constructor.
 		/// </summary>
 		public EmbeddedResourcesStorage()
-			: this(Assembly.GetEntryAssembly())
+			: this(Assembly.GetEntryAssembly(), string.Empty)
+		{
+		}
+
+		/// <summary>
+		/// Constructor.
+		/// </summary>
+		public EmbeddedResourcesStorage(Assembly assembly)
+			: this(assembly, string.Empty)
+		{
+		}
+
+		/// <summary>
+		/// Constructor.
+		/// </summary>
+		public EmbeddedResourcesStorage(string baseNamespace)
+			: this(Assembly.GetEntryAssembly(), baseNamespace)
 		{
 		}
 
@@ -20,17 +42,26 @@ namespace Snowball.Storage
 		/// Constructor.
 		/// </summary>
 		/// <param name="assembly"></param>
-		public EmbeddedResourcesStorage(Assembly assembly)
+		public EmbeddedResourcesStorage(Assembly assembly, string baseNamespace)
 		{
 			if (assembly == null)
 				throw new ArgumentNullException("assembly");
 
 			this.assembly = assembly;
+			this.BaseNamespace = baseNamespace;
 		}
 
 		public Stream GetStream(string fileName)
 		{
-			return this.assembly.GetManifestResourceStream("fileName");
+			if (this.BaseNamespace != null && this.BaseNamespace.Length > 0)
+				fileName = this.BaseNamespace + "." + fileName;
+
+			Stream stream = this.assembly.GetManifestResourceStream(fileName);
+
+			if (stream == null)
+				throw new FileNotFoundException("Unable to get Stream to \"" + fileName + "\".");
+
+			return stream;
 		}
 	}
 }
